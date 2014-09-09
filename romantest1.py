912e51291be4733c8f632d19dc878b929e61060e
Felipe Miramontes Romero <felipemiramontesr@gmail.com>
2014-09-09 18:32:08 -0500

    Sexto paso

--- romantest1.py
+++ romantest1.py
@@ -65,11 +65,19 @@ class KnownValues(unittest.TestCase):
             result = roman1.to_roman(integer)      
             self.assertEqual(numeral, result)      
 
-class ToRomanBadInput(unittest.TestCase):         
+class ToRomanBadInput(unittest.TestCase):
 
-    def test_too_large(self):                                             
+    def test_too_large(self):
         '''to_roman should fail with large input'''
         self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, 4000)  
+
+    def test_zero(self):
+        '''to_roman should fail with 0 input'''
+        self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, 0)     
+
+    def test_negative(self):
+        '''to_roman should fail with negative input'''
+        self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, -1)   
 
 if __name__ == '__main__':
     unittest.main()
