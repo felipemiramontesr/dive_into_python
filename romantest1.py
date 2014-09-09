132b5efc443a841ac52d11c8c4fec9b7deef7b48
Felipe Miramontes Romero <felipemiramontesr@gmail.com>
2014-09-09 17:52:17 -0500

    Tercer paso -b-

--- romantest1.py
+++ romantest1.py
@@ -69,7 +69,7 @@ class ToRomanBadInput(unittest.TestCase):
 
     def test_too_large(self):                                             
         '''to_roman should fail with large input'''
-        self.assertRaises(roman1.OutOfRangeError, roman2.to_roman, 4000)  
+        self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, 4000)  
 
 roman1.to_roman(4000)
 roman1.to_roman(5000)
